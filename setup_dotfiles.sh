5edc0b70627d8b65c9a1533efff93ea4f526cd78
abargiela <abargiela@heineken.globoprdom.com.br>
2018-05-30 12:31:15 -0300

    Add more paths

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,10 +1,14 @@
 REPO_PATH="${HOME}/Documents/dotfiles/"
 
-mv ${HOME}/.bashrc ${HOME}/bashrc.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
-mv ${HOME}/.vimrc ${HOME}/vimrc.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
-mv ${HOME}/.tmux.conf ${HOME}/tmux.conf.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+mv ${HOME}/.bashrc      ${HOME}/bashrc.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+mv ${HOME}/.vimrc       ${HOME}/vimrc.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+mv ${HOME}/.tmux.conf   ${HOME}/tmux.conf.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+mv ${HOME}/.fluxbox     ${HOME}/.fluxbox-$(date '+%Y-%m-%d_%H-%M-%S')
+mv ${HOME}/.i3          ${HOME}/.i3-$(date '+%Y-%m-%d_%H-%M-%S')
 
-ln -s ${REPO_PATH}/.bashrc ${HOME}/
-ln -s ${REPO_PATH}/.vimrc ${HOME}/
-ln -s ${REPO_PATH}/.personal.alias ${HOME}/
-ln -s ${REPO_PATH}/.tmux.conf ${HOME}/
+ln -s ${REPO_PATH}/.bashrc          ${HOME}/
+ln -s ${REPO_PATH}/.vimrc           ${HOME}/
+ln -s ${REPO_PATH}/.personal.alias  ${HOME}/
+ln -s ${REPO_PATH}/.tmux.conf       ${HOME}/
+ln -s ${REPO_PATH}/.fluxbox         ${HOME}/ 
+ln -s ${REPO_PATH}/.i3/             ${HOME}/
