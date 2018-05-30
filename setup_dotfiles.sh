6f226b5d83330fa87f85ef9b11e4627a079eddca
abargiela <abargiela@heineken.globoprdom.com.br>
2018-05-30 12:14:26 -0300

    setup script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,7 +6,5 @@ mv ${HOME}/.tmux.conf ${HOME}/tmux.conf.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
 
 ln -s ${REPO_PATH}/.bashrc ${HOME}/
 ln -s ${REPO_PATH}/.vimrc ${HOME}/
-ln -s ${REPO_PATH}/.work.alias ${HOME}/
 ln -s ${REPO_PATH}/.personal.alias ${HOME}/
 ln -s ${REPO_PATH}/.tmux.conf ${HOME}/
-
