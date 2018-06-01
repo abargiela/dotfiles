c7ef4c9a984a8b5f29586166a838dc3cc48426aa
abargiela <abargiela@heineken.globoprdom.com.br>
2018-06-01 15:45:40 -0300

    Add plugins

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -5,6 +5,6 @@ FILES=".bashrc .vimrc .tmux.conf .fluxbox .i3"
 
 for i in ${FILES}; do
     unlink  ${HOME}/${i}
-    mv  ${HOME}/.${i}   ${HOME}/${i}.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+    mv  ${HOME}/${i}   ${HOME}/${i}.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
     ln -s   ${REPO_PATH}/${i}   ${HOME}/
 done
