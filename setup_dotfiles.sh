7b63f7e831b7cdc87eb697848dd58c6fe57d4c8e
abargiela <abargiela@heineken.globoprdom.com.br>
2018-06-01 15:55:24 -0300

    Add for

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,7 +4,11 @@ REPO_PATH="${HOME}/Documents/dotfiles/"
 FILES=".bashrc .vimrc .tmux.conf .fluxbox .i3"
 
 for i in ${FILES}; do
-    unlink  ${HOME}/${i}
-    mv  ${HOME}/${i}   ${HOME}/${i}.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+    if [[ -h ${HOME}/${i} ]];then
+        unlink  ${HOME}/${i}
+    fi 
+    if [[ -f ${HOME}/${i} ]]; then
+        mv  ${HOME}/${i}   ${HOME}/${i}.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
+    fi
     ln -s   ${REPO_PATH}/${i}   ${HOME}/
 done
