f433fc7676a1ba0a71b28eae382972166b557c59
Alexandre Bargiela <alexandre.bargiela@C02TQ184J1WL.local>
2019-12-10 12:22:26 -0300

    Fix paths files

---
+++ init_macOS.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+function install_brew() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+function install_brew_packages() {
+    DEFAULT=(
+        'zsh' \
+        'ansible' \
+        'neovim' \
+        'python3' \
+        'git' \
+        'tmux'
+    )
+    CASKS=(
+        'visual-studio-code' \
+        'google-chrome' \
+        'iterm2' \
+        'microsoft-teams' \
+        'forticlient' \
+        'enpass' \
+        'rambox'
+    )
+
+    for brew_default in ${DEFAULT[@]}; do 
+        brew install  ${brew_default}
+    done
+    for brew_casks in ${CASKS[@]}; do 
+        brew cask install ${brew_casks}
+    done
+}
+
+function install_pip_packages() {
+    pip3 install virtualenv
+}
+
+function install_neovim_plug(){
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+function create_virtualenv(){
+    virtualenv -p python3 ~/Documents/venv_py3
+}
+
+function dot_files_repo_download(){
+    # If you're not me, comment git and use https :P 
+    #git clone https://github.com/abargiela/dotfiles.git ${HOME}/Documents/dotfiles/
+    git clone git@github.com:abargiela/dotfiles.git ${HOME}/Documents/dotfiles/
+}
+
+function dot_files_repo_install(){
+    bash ${HOME}/Documents/dotfiles/setup_dotfiles.sh
+}
+function neovim_install_plugins(){
+    nvim +'PlugInstall --sync' +'PlugUpdate' +qa
+}
+function zsh_load_config(){
+    source ~/.zshrc
+}
+
+
+function main() {
+
+}
+
+
+
+main
