658c711337952eff66f5144bf8fa31165904fd25
Alexandre Bargiela <alexandre.bargiela@C02TQ184J1WL.local>
2019-12-10 11:47:17 -0300

    Add other files

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 REPO_PATH="${HOME}/Documents/dotfiles/"
 
+# Base configs
 FILES=".bashrc .vimrc .tmux.conf .fluxbox .i3 .Xresources .zshrc"
-NEOVIM_CONFIG_FILE="init.vim"
-VSCODE_CONFIG_FILE="settings.json"
-
 for i in ${FILES}; do
     if [[ -h ${HOME}/${i} ]];then
         unlink ${HOME}/${i}
@@ -18,6 +16,7 @@ for i in ${FILES}; do
 done
 
 #NeoVim config
+NEOVIM_CONFIG_FILE="init.vim"
 if [[ -h ${REPO_PATH}/${NEOVIM_CONFIG_FILE} ]];then
     unlink ${REPO_PATH}/${NEOVIM_CONFIG_FILE}
 fi 
@@ -25,7 +24,9 @@ if [[ -f ${REPO_PATH}/${NEOVIM_CONFIG_FILE} ]]; then
     mv  ${REPO_PATH}/${NEOVIM_CONFIG_FILE} ${REPO_PATH}/${NEOVIM_CONFIG_FILE}.bkp-$(date '+%Y-%m-%d_%H-%M-%S')
     ln -s ${REPO_PATH}/${NEOVIM_CONFIG_FILE} ~/.config/nvim/
 fi
+
 # VSCode config
+VSCODE_CONFIG_FILE="settings.json"
 if [[ -h ${REPO_PATH}/${VSCODE_CONFIG_FILE} ]];then
     unlink ${REPO_PATH}/${VSCODE_CONFIG_FILE}
 fi
